91d683f0713d14f5260316179865dc63d19b23f6
rhayeksa <rhayeksads@gmail.com>
2021-02-17 17:34:02 +0700

    remove strictmode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, StrictMode } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import routes from "./configs/routes";
 import "./App.css";
@@ -7,20 +7,22 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div hidden> learn react</div>
-        <Switch>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              exact
-              render={(props) => (
-                // <route.component {...props} {...route.props} />
-                <route.component />
-              )}
-            />
-          ))}
-        </Switch>
+        <StrictMode>
+          <div hidden> learn react</div>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact
+                render={(props) => (
+                  // <route.component {...props} {...route.props} />
+                  <route.component />
+                )}
+              />
+            ))}
+          </Switch>
+        </StrictMode>
       </Router>
     );
   }
